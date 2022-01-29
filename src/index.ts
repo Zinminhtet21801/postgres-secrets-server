5aade49f83cb90c86640b4550e81913e079462f9
Zin Min Htet <zetminhtin@gmail.com>
2022-01-29 14:27:34 +0630

    shits

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,25 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { Secrets } from "./entity/Secrets";
 
+const express = require("express");
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+const app = express();
+const fs = require("fs");
+const https = require("https");
+const privateKey = fs.readFileSync("./server.key");
+const certificate = fs.readFileSync("./server.cert");
+var credentials = { key: privateKey, cert: certificate };
+var httpsServer = https.createServer(credentials, app);
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+httpsServer.listen(PORT, () => console.log("SHIT"));
+
 createConnection()
   .then(async (connection) => {
-    const express = require("express");
-    const cors = require("cors");
-    const PORT = process.env.PORT || 5000;
-    const app = express();
-    app.use(cors());
-
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-
     app.get("/", async (req: Request, res: Response) => {
       const secret = new Secrets();
       const result = await connection.manager.find(Secrets);
@@ -21,18 +29,16 @@ createConnection()
     });
 
     app.post("/", async (req: Request, res: Response) => {
-        console.log(req.body);
-        
+      console.log(req.body);
+
       const { title, body } = req.body;
-      
+
       console.log("Inserting a new user into the database...");
       const secret = new Secrets();
       secret.title = title;
       secret.body = body;
       const saveSecret = await connection.manager.save(secret);
-      res.json(saveSecret)
+      res.json(saveSecret);
     });
-
-    app.listen(PORT, () => console.log("SHIT"));
   })
   .catch((error) => console.log(error));
