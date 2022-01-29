4d023c36db75d99bbf05b01d1f251461da4aef23
Zin Min Htet <zetminhtin@gmail.com>
2022-01-29 20:59:54 +0630

    connect database

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,63 @@
+require("dotenv").config();
 import { Request, Response } from "express";
 import "reflect-metadata";
-import { createConnection } from "typeorm";
+import {
+  createConnection,
+  ConnectionOptions,
+  getConnectionOptions,
+} from "typeorm";
 import { Secrets } from "./entity/Secrets";
 
 const express = require("express");
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const app = express();
-const fs = require("fs");
-const https = require("https");
-const privateKey = fs.readFileSync("./server.key");
-const certificate = fs.readFileSync("./server.cert");
-var credentials = { key: privateKey, cert: certificate };
-var httpsServer = https.createServer(credentials, app);
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-httpsServer.listen(PORT, () => console.log("SHIT"));
+const https = require("https");
+const fs = require("fs");
 
-createConnection()
+const options = {
+  key: fs.readFileSync("key.pem"),
+  cert: fs.readFileSync("cert.pem"),
+};
+
+https
+  .createServer(options, function (req: Request, res: Response) {
+    res.writeHead(200);
+    res.end("hello world\n");
+  })
+  .listen(5000);
+
+const getOptions = async () => {
+  let connectionOptions: ConnectionOptions;
+  connectionOptions = {
+    type: "postgres",
+    synchronize: false,
+    logging: false,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+    entities: ["entity/**/*.ts"],
+  };
+  if (process.env.DATABASE_URL) {
+    Object.assign(connectionOptions, { url: process.env.DATABASE_URL });
+  } else {
+    connectionOptions = await getConnectionOptions();
+  }
+
+  return connectionOptions;
+};
+
+const connect2Database = async (): Promise<any> => {
+  const typeormconfig = await getOptions();
+  await createConnection(typeormconfig);
+};
+
+connect2Database()
   .then(async (connection) => {
     app.get("/", async (req: Request, res: Response) => {
       const secret = new Secrets();
