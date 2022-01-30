51a3bc1ac981590595e7b00a2952b2364e59c4b0
Zin Min Htet <zetminhtin@gmail.com>
2022-01-31 00:19:40 +0630

    ok

--- src/entity/Secrets.ts
+++ src/entity/Secrets.ts
@@ -2,7 +2,7 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 @Entity()
 export class Secrets {
-  @PrimaryGeneratedColumn("rowid")
+  @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
